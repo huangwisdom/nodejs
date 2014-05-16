3fee82a058f7be66f673163437b3529f734a4700
huangwisdom <huangwisdom@163.com>
2014-05-16 09:49:22 +0800

    Create TableConfig.java

---
+++ src/com/wisdom/code/config/TableConfig.java
@@ -0,0 +1,48 @@
+package com.wisdom.code.config;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+public class TableConfig {
+
+    @XStreamAlias("table-name")
+    private String tableName;
+
+    @XStreamAlias("package-extend")
+    private String packageExtend;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getPackageExtend() {
+        return packageExtend;
+    }
+
+    public void setPackageExtend(String packageExtend) {
+        this.packageExtend = packageExtend;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
