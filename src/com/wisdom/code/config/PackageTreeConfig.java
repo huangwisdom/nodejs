8f1c58fc4c97d966d7c4972c2e7db34b5fd30c8f
huangwisdom <huangwisdom@163.com>
2014-05-16 09:48:55 +0800

    Create PackageTreeConfig.java

---
+++ src/com/wisdom/code/config/PackageTreeConfig.java
@@ -0,0 +1,94 @@
+package com.wisdom.code.config;
+
+public class PackageTreeConfig {
+    
+    private String dto;
+    
+    private String dao;
+    
+    private String daoimpl;
+    
+    private String sqlmap;
+    
+    private String service;
+    
+    private String serviceimpl;
+    
+    private String action;
+    
+    private String controller;
+    
+    private String form;
+
+    public String getDto() {
+        return dto;
+    }
+
+    public void setDto(String dto) {
+        this.dto = dto;
+    }
+
+    public String getDao() {
+        return dao;
+    }
+
+    public void setDao(String dao) {
+        this.dao = dao;
+    }
+
+    public String getDaoimpl() {
+        return daoimpl;
+    }
+
+    public void setDaoimpl(String daoimpl) {
+        this.daoimpl = daoimpl;
+    }
+
+    public String getSqlmap() {
+        return sqlmap;
+    }
+
+    public void setSqlmap(String sqlmap) {
+        this.sqlmap = sqlmap;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getServiceimpl() {
+        return serviceimpl;
+    }
+
+    public void setServiceimpl(String serviceimpl) {
+        this.serviceimpl = serviceimpl;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getController() {
+        return controller;
+    }
+
+    public void setController(String controller) {
+        this.controller = controller;
+    }
+    
+    public String getForm() {
+        return form;
+    }
+
+    public void setForm(String form) {
+        this.form = form;
+    }
+}
